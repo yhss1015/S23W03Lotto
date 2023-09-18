fbb4c61643b3e081eb244307ecb5f87ad86ded4e
root <root@DESKTOP-2I57GUG.localdomain>
2023-09-18 15:56:03 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20180594/MainActivity.kt
@@ -0,0 +1,26 @@
+package kr.ac.kumoh.ce.s20180594
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.ce.s20180594.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView((main.root))
+
+        main.btnGenerate.setOnClickListener{
+            main.num1.text = Random.nextInt(1,46).toString()
+            main.num2.text = Random.nextInt(1,46).toString()
+            main.num3.text = Random.nextInt(1,46).toString()
+            main.num4.text = Random.nextInt(1,46).toString()
+            main.num5.text = Random.nextInt(1,46).toString()
+            main.num6.text = Random.nextInt(1,46).toString()
+        }
+
+    }
+}
